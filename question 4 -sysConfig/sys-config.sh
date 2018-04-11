5264664d4c3b6507aac9536a8c3ae01c673dbe33
slaybhi <a.abhijith1adoor@gmail.com>
2018-04-11 18:44:33 +0530

    added question no 4 from syllabus

---
+++ question 4 -sysConfig/sys-config.sh
@@ -0,0 +1,6 @@
+echo "currently logged in user : "$(whoami)
+echo "currently using shell: "$SHELL
+echo "home directory :"$HOME
+echo "operating system: "$(uname -o)
+echo "current working directory"$(pwd)
+echo "current  PATH settings (enviornment variables)" $PATH
